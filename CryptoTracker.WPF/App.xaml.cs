c10c68dd8b61c139455c315213e110e664d06b70
NazarPetrakov <nazarpetrakov1@gmail.com>
2026-02-08 16:25:54 +0200

    chore(project): configure .NET Host, DI

--- CryptoTracker.WPF/App.xaml.cs
+++ CryptoTracker.WPF/App.xaml.cs
@@ -1,12 +1,34 @@
-﻿using System.Windows;
+﻿using CryptoTracker.WPF.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace CryptoTracker.WPF
 {
     public partial class App : Application
     {
+        private IHost _host;
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services => services.AddServices())
+                .Build();
+        }
         protected override void OnStartup(StartupEventArgs e)
         {
+            _host.Start();
+
+            MainWindow mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
             base.OnStartup(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.StopAsync().GetAwaiter().GetResult();
+            _host.Dispose();
+
+            base.OnExit(e);
         }
     }
 

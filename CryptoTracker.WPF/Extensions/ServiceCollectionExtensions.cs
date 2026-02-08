e0e816a53b29978fd862c7e85677f7433afd6dd6
NazarPetrakov <nazarpetrakov1@gmail.com>
2026-02-08 16:26:49 +0200

    chore(di): register MainWindow and ViewModels

---
+++ CryptoTracker.WPF/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CryptoTracker.WPF.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptoTracker.WPF.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        internal static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<HomeViewModel>();
+
+            return services;
+        }
+    }
+}
